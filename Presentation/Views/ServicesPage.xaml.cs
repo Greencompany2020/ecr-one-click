b7cfb4af1bad4e3a1c69caaf9bd1301a9480ebc9
JoshAlexis <joshyelc@gmail.com>
2024-08-14 14:04:09 -0700

    feat: add logic to fetch services list
    - Removed bindind context type assignation in xaml file

--- Presentation/Views/ServicesPage.xaml.cs
+++ Presentation/Views/ServicesPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using EcrOneClick.DI;
+﻿using EcrOneClick.DI;
 using EcrOneClick.Presentation.ViewModels;
 
 namespace EcrOneClick.Presentation.Views;
@@ -15,5 +10,12 @@ public partial class ServicesPage : ContentPage
         InitializeComponent();
 
         BindingContext = ServiceHelper.GetService<ServicesViewModel>();
+    }
+    
+    protected override void OnAppearing()
+    {
+        var viewModel = (ServicesViewModel)BindingContext;
+        
+        viewModel.LoadServices();
     }
 }
