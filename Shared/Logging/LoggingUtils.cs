b2c6f6be106c113306937e90e70ee23c04a16b10
JoshAlexis <joshyelc@gmail.com>
2024-08-14 14:01:38 -0700

    refactor: create folder for shared elements

---
+++ Shared/Logging/LoggingUtils.cs
@@ -0,0 +1,14 @@
+﻿using FluentResults;
+
+namespace EcrOneClick.Logging;
+
+public static class LoggingUtils
+{
+    public static string FetchErrorsText(List<IError> errors)
+    {
+        var errorMessages = new List<string>();
+        
+        errors.ForEach(e => errorMessages.Add(e.Message));
+        return string.Join(" | ", errorMessages);
+    }
+}
