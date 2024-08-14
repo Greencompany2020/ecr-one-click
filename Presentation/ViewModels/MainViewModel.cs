0281a0cb271801a401729cf7a40a4774cc7cd306
JoshAlexis <joshyelc@gmail.com>
2024-08-14 09:27:46 -0700

    feat: save docker swarm status

--- Presentation/ViewModels/MainViewModel.cs
+++ Presentation/ViewModels/MainViewModel.cs
@@ -1,5 +1,7 @@
 using EcrOneClick.Infrastructure.Abstract;
 using EcrOneClick.Presentation.Abstract;
+using EcrOneClick.UseCases.Abstract;
+using EcrOneClick.UseCases.Request;
 using Microsoft.Extensions.Logging;
 
 namespace EcrOneClick.Presentation.ViewModels;
@@ -7,27 +9,50 @@ namespace EcrOneClick.Presentation.ViewModels;
 public class MainViewModel : IBaseViewModel
 {
     private readonly IDockerService _dockerService;
-
+    
     private readonly ILogger<MainViewModel> _logger;
     
-    public MainViewModel(IDockerService dockerService, ILogger<MainViewModel> logger)
+    private readonly IGetSwarmMode _getSwarmMode;
+
+    private readonly ISetSwarmMode _setSwarmMode;
+    
+    public MainViewModel(
+        IDockerService dockerService,
+        ILogger<MainViewModel> logger,
+        IGetSwarmMode getSwarmMode,
+        ISetSwarmMode setSwarmMode
+        )
     {
         _dockerService = dockerService;
         _logger = logger;
+        _getSwarmMode = getSwarmMode;
+        _setSwarmMode = setSwarmMode;
     }
 
     public async void BeginSwarmMode()
     {
-        if (!_dockerService.IsInSwarmMode)
+        var swarmMode = _getSwarmMode.Execute();
+
+        if (swarmMode.IsFailed)
+        {
+            await Shell.Current.DisplayAlert("Error", "No se pudo obtener configuracion de docker", "OK");
+            _logger.LogError("[{Model}.{Method}]: {Error}", nameof(MainViewModel), nameof(BeginSwarmMode), string.Join(",", swarmMode.Errors));
+            return;            
+        }
+        
+        if (!swarmMode.Value)
         {
             var result = await _dockerService.BeginSwarmMode();
 
             if (result.IsFailed)
             {
+                _setSwarmMode.Execute(new (false));
+                
                 _logger.LogInformation("[{Model}.{Method}]: Cannot init Docker Swarm Mode. Using default containers mode", nameof(MainViewModel), nameof(BeginSwarmMode));
             }
             else
             { 
+                _setSwarmMode.Execute(new (true));
                 _logger.LogInformation("[{Model}.{Method}]: Swarm Mode initialized", nameof(MainViewModel), nameof(BeginSwarmMode));
             }
         }
